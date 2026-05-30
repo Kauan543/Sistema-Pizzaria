0b1b280cb66cfdc485e1b34d6a983a59b21e228e
Kauan Calegari <kauancalegari@ufpr.br>
2026-05-30 11:47:35 -0300

    Primeira tela

---
+++ ProjetoFinalPizzaria/src/main/java/com/mycompany/projetofinalpizzaria/ProjetoFinalPizzaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetofinalpizzaria;
+
+/**
+ *
+ * @author Kauan
+ */
+public class ProjetoFinalPizzaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
